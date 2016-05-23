c68162eda3166bcdef197fa81ceb9aa5e1ed7be4
Vishal Gaurav <vishal.gaurav@hotmail.com>
2016-05-23 14:56:53 -0400

    updated project structure

---
+++ src/peer_runner.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : peer_runnner.c
+ Author      : Vishal Gaurav
+ Version     :
+ Copyright   : CS60 dartsync project
+ Description : this file will contain the main function for peer
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World PEER!!!");
+	return EXIT_SUCCESS;
+}
--- src/tracker_runner.c
+++ src/tracker_runner.c
@@ -1,10 +1,10 @@
 /*
  ============================================================================
- Name        : dartsync.c
+ Name        : tracker_runnner.c
  Author      : Vishal Gaurav
  Version     :
  Copyright   : CS60 dartsync project
- Description : Hello World in C, Ansi-style
+ Description : this file will contain the main function for tracker
  ============================================================================
  */
 
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World TRACKER!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
